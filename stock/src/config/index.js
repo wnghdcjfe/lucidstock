be3f4976bfbfd944b89f56d529c57742130b13b6
hongchul.ju <hongchul.ju@navercorp.com>
2021-02-07 17:09:24 +0900

    da

---
+++ stock/src/config/index.js
@@ -0,0 +1,4 @@
+///const test_serever = "http://127.0.0.1/"
+const real_server = "https://lucidstock.herokuapp.com/"
+export const todayurl = `${real_server}stocks/today` 
+export const daysurl = `${real_server}stocks/days`  
